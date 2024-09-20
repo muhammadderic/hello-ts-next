227a8ff1a5030747d03eab01ea0901b5bf3f7ac1
muhammadderic <muhammad.donny.e@gmail.com>
2024-09-20 11:00:43 +0700

    Initialize auth js, set adapter and provider

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./lib/prisma"
+import { Adapter } from "next-auth/adapters"
+import Google from "next-auth/providers/google"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [Google],
+})
